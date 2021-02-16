633cf1b10a2b8a7117a2c8cec78ffbf811f3439e
Kenechukwu Okoye <mr.kene.okoye@gmail.com>
2021-02-16 23:34:10 +0100

    Navbar updated

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import Home from './Components/Home/Home';
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import About from './Components/AboutMe/About';
-import Contact from './Components/Contact/Contact';
-import Footer from './Components/Footer/Footer';
-import Portfolio from './Components/Portfolio/Portfolio';
-
+import React from "react";
+import Home from "./Components/Home/Home";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import About from "./Components/AboutMe/About";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
+import Portfolio from "./Components/Portfolio/Portfolio";
 
 const App = () => {
-
   return (
     <>
-      <div className='container'>
-        <Home/>
-        <Navbar/>
-        <About/>
+      <div className="container">
+        <Home />
+        <Navbar />
+        <About />
         <Portfolio />
-        <Contact/>
-        <Footer/>
+        <Contact />
+        <Footer />
       </div>
     </>
   );
-}
+};
 
 export default App;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,66 +1,61 @@
-import React from 'react';
+import React from "react";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import myLogo from '../../Assets/Images/myLogo-2.png';
-import MobileMenu from './MobileMenu';
-import DesktopMenu from './DesktopMenu';
-import LanguageMenu from './LanguageMenu';
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import myLogo from "../../Assets/Images/myLogo.png";
+import MobileMenu from "./MobileMenu";
+import DesktopMenu from "./DesktopMenu";
+import LanguageMenu from "./LanguageMenu";
 
 // START Navbar.js STYLES //
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
+  root: {
+    flexGrow: 1,
+  },
 
-    myLogo: {
-        height: '50px',
-        width: '50px',
-    },
-    
-    toolBarOptions: {
-        paddingRight: theme.spacing(5),
-        paddingLeft: theme.spacing(5),
-        justifyContent: 'space-between',
-        minHeight: '11px',
-        background: 'rgb(37, 37, 37)',
-    },
+  myLogo: {
+    height: "35px",
+    width: "35px",
+  },
 
-    navList: {
-        display: 'flex',
-        justifyContent: 'center'
-    }
-}))
+  toolBarOptions: {
+    paddingRight: theme.spacing(5),
+    paddingLeft: theme.spacing(5),
+    justifyContent: "space-between",
+    minHeight: "11px",
+    background: "rgb(37, 37, 37)",
+  },
+
+  navList: {
+    display: "flex",
+    justifyContent: "center",
+  },
+}));
 // END Navbar.js STYLES //
 
 const Navbar = () => {
-    const classes = useStyles();
-    const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
+  const classes = useStyles();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
 
-    return (
-        <>
-        <div id="myHeader" className={classes.root}>
-            <AppBar position="fixed">
-                <Toolbar className={classes.toolBarOptions}>
-                    <img src={myLogo} alt='my logo' className={classes.myLogo}/>
-                    <div className={classes.navList}>
-                        <LanguageMenu/>
-                        
-                        { isMobile ? (
-                            <MobileMenu/>
-                        ) : ( 
-                            <DesktopMenu/>
-                        )}
-                    </div>
-                    
-                </Toolbar>
-            </AppBar>
-        </div>
+  return (
+    <>
+      <div id="myHeader" className={classes.root}>
+        <AppBar position="fixed">
+          <Toolbar className={classes.toolBarOptions}>
+            <img src={myLogo} alt="my logo" className={classes.myLogo} />
+            <div className={classes.navList}>
+              <LanguageMenu />
+
+              {isMobile ? <MobileMenu /> : <DesktopMenu />}
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
     </>
-    )
-}
+  );
+};
 
 export default Navbar;
